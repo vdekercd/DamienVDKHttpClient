75620af4535fa904c24be6c9c369a771a4807c92
Damien Vande Kerckhove <damien.vandekerckhove@gmail.com>
2022-01-07 20:52:03 +0100

    --

--- DamienVDK.HttpClient/Services/NamedHttpClientService.cs
+++ DamienVDK.HttpClient/Services/NamedHttpClientService.cs
@@ -1,30 +1,29 @@
-﻿namespace DamienVDKHttpClient.Services
+﻿namespace DamienVDKHttpClient.Services;
+
+public class NamedHttpClientService
 {
-    public class NamedHttpClientService
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public NamedHttpClientService(IHttpClientFactory httpClientFactory)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
+        _httpClientFactory = httpClientFactory;
+    }
 
-        public NamedHttpClientService(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
+    public async Task PostTodoAsync(Todo todo)
+    {
+        var httpClient = _httpClientFactory.CreateClient(Constants.HttpClientName);
+        var content = new StringContent(JsonConvert.SerializeObject(todo), Encoding.UTF8, "application/json");
+        var response = await httpClient.PostAsync($"V1/Todo", content);
+        Console.WriteLine($"Status code : {response.StatusCode}");
+        response.EnsureSuccessStatusCode();
+    }
 
-        public async Task PostTodoAsync(Todo todo)
-        {
-            var httpClient = _httpClientFactory.CreateClient(Constants.HttpClientName);
-            var content = new StringContent(JsonConvert.SerializeObject(todo), Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync($"V1/Todo", content);
-            Console.WriteLine($"Status code : {response.StatusCode}");
-            response.EnsureSuccessStatusCode();
-        }
-
-        public async Task<string> GetTodoAsync()
-        {
-            var httpClient = _httpClientFactory.CreateClient(Constants.HttpClientName);
-            var response = await httpClient.GetAsync("V1/Todo");
-            Console.WriteLine(response.StatusCode);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
-        }
+    public async Task<string> GetTodoAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient(Constants.HttpClientName);
+        var response = await httpClient.GetAsync("V1/Todo");
+        Console.WriteLine(response.StatusCode);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
     }
 }
